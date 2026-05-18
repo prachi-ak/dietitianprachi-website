da21acc78da588351273d8c83801270b74af8212
Sarvaarth Narang <sarvaarth@hotmail.com>
2026-05-18 11:13:37 +0530

    Fix Stripe init to not throw at build time

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,5 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY environment variable is not set.');
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2024-06-20',
 });
